ea9d078641e317d1d4676c7a7a57ea768a195734
mdmac1983 <mdmac1983@proton.me>
2026-08-15 18:33:50 -0400

    Create AppInfo.kt

---
+++ app/src/main/java/com/calculator/vault/model/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.calculator.vault.model
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val name: String,
+    val packageName: String,
+    val icon: Drawable
+)
